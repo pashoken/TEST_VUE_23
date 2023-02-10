a4f069678fc50e88b4889b3a2e4342a287d74fb7
pashoken <pashaperevalov6@gmail.com>
2023-02-10 23:08:06 +0400

    df

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<draggable v-model="newArr" group="people" @start="drag=true" @end="drag=false">
+   <div v-for="(element,index) in newArr" :key="index">{{element}}</div>
+</draggable>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  componets:{
+    draggable,
+  },
+  data() {
+    return {
+      arr: 'слово',
+      newArr: [],
+      drag: false,
+    };
+  },
+
+  mounted() {
+    this.newArr = this.arr.split('').sort((qw) => Math.random() - 0.5);
+    console.log(this.newArr);
+  },
+  methods: {
+    asd(){
+      console.log("asdsadasdsad");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+.qwe 
+  display: flex
+  width: 50vw
+  height: 300px
+  align-self: center
+  justify-content: space-between
+  align-items: center
+  
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios, {isCancel, AxiosError} from 'axios'
+import './assets/main.css'
+
+createApp(App).mount('#app')
